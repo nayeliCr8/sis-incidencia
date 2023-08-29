754550a91515812df25dfb0a0ad432f1f942fd8f
yerson joel fonseca lizarraga <yersonjoelfl@gmail.com>
2023-08-28 22:09:05 -0500

    .__

---
+++ resources/js/Components/TableDataCell.vue
@@ -0,0 +1,6 @@
+<template>
+    <th class="px-6 py-4">
+      
+      <slot />
+    </th>
+  </template>
--- routes/admin.php
+++ routes/admin.php
@@ -2,15 +2,23 @@
 
 use App\Http\Controllers\EtiquetaController;
 use App\Http\Controllers\IncidenciaController;
+use App\Http\Controllers\PermissionController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 
+Route::resource('users', UserController::class);
+Route::resource('roles', RoleController::class);
+Route::resource('permissions', PermissionController::class);
 
 Route::get('prueba', function(){
-    return Inertia::render('admin/adminhome');
-})->name('prueba');
+    return Inertia::rernde('admin/adminhome');
+})->name('prueba')->middleware(['auth','role:superadmin']);
 
-Route::resource('etiqueta', EtiquetaController::class);
-Route::resource('incidencia', IncidenciaController::class);
+Route::middleware(['auth'])->group(function () {
+    Route::resource('etiqueta', EtiquetaController::class);
+    Route::resource('incidencia',IncidenciaController::class);
+});
