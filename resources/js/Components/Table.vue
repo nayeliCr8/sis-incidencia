bcfcdaf0a9fd502488b440f2e0cbe1c7486b9134
Cristiancg <45461497+armindnwyr@users.noreply.github.com>
2023-08-30 00:14:53 -0500

    change component table

--- resources/js/Components/Table.vue
+++ resources/js/Components/Table.vue
@@ -2,11 +2,11 @@
 </script>
 <template>
   <div class="overflow-hidden rounded-lg border border-gray-200 shadow-md m-5">
-    <table class="max-w-full border-collapse bg-white text-left text-sm text-gray-500">
+    <table class="w-full border-collapse bg-white text-left text-sm text-gray-500">
       <thead class="bg-gray-50">
         <slot name="header"/>
       </thead>
-      <tbody class="divide-gray-100 border-t border-gray-100">
+      <tbody class="divide-y divide-gray-100 border-t border-gray-100">
         <slot/>
       </tbody>
     </table>
